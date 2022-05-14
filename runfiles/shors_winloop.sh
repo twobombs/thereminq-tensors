d602e2c597b9d796011f2b6e300a10657d5eec45
Aryan Blaauw <ablaauw@gmx.net>
2022-05-14 19:46:02 +0200

    loop integration

--- runfiles/shors_winloop.sh
+++ runfiles/shors_winloop.sh
@@ -8,10 +8,10 @@ for i in {1..55108}
 	prime2=$(matho-primes -c 1 -u $i2)
 	echo $(($prime * $prime2)) > fact
 	fact=$(<fact)
-	echo $fact | ./shors_factoring $1 > factoring
-	cat factoring | grep Found
+	sed -i "s/toFactor=.*/toFactor=$fact/g" /pyqrack-jupyter/shor.py
+	ipython3 /pyqrack-jupyter/shor.py > factoring
+	cat factoring | grep 'Factors found'
 	cat factoring | grep Failed
-	cat factoring | grep 'non- relative prime'
 	echo 'Prime #1:   ' $prime 'Prime #2:   ' $prime2 'Multiplied: ' $fact
 	echo ' '
     done
