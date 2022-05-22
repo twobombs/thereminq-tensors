32cf475682d11e3f4ef5f2487f7db1864371c54d
Aryan Blaauw <ablaauw@gmx.net>
2022-05-22 16:11:59 +0000

    RSA (t)weak

--- runfiles/shors_qimcifa_loop.sh
+++ runfiles/shors_qimcifa_loop.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # qimcifa winloop 32 bits range with 16 bit steps
 #
-for i in {1..4294967295..65535}
+for i in {65535..4294967295..65535}
     do
 	prime=$(matho-primes -c 1 -u $i)
-	echo $(($prime * $prime)) > i2
+	echo $(($prime + $prime)) > i2
 	i2=$(<i2)
 	prime2=$(matho-primes -c 1 -u $i2)
 	echo $(($prime * $prime2)) > fact
