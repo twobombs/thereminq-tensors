585a53236292df329da76e07ecc16037e99c9f52
Aryan Blaauw <ablaauw@gmx.net>
2022-05-22 16:54:36 +0200

    python and C winloops
    guess who wins...

---
+++ runfiles/shors_qimcifa_loop.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# qimcifa winloop 32 bits range with 16 bit steps
+#
+for i in {1..4294967295..65535}
+    do
+	prime=$(matho-primes -c 1 -u $i)
+	echo $(($prime * $prime)) > i2
+	i2=$(<i2)
+	prime2=$(matho-primes -c 1 -u $i2)
+	echo $(($prime * $prime2)) > fact
+	fact=$(<fact)
+	echo $fact | /qimcifa/qimcifa $1 > qimfact
+	cat qimfact | grep 'Success: Found'
+done
