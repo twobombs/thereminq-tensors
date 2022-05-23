f9407a65bc17bf6ef4d2abec867a542db73bec9d
Aryan Blaauw <ablaauw@gmx.net>
2022-05-23 16:41:01 +0000

    modified for RSA compliance

--- runfiles/shors_winloop.sh
+++ runfiles/shors_winloop.sh
@@ -1,17 +1,14 @@
-for i in {1..55108}
-    do
-	for j in {1..10}
+for i in {65535..4294967295..255}
 	do
-	prime=$(matho-primes -c 1 -u $i)
-	echo $(($prime * $prime)) > i2
-	i2=$(<i2)
-	prime2=$(matho-primes -c 1 -u $i2)
-	echo $(($prime * $prime2)) > fact
-	fact=$(<fact)
-	sed -i "s/toFactor=.*/toFactor=$fact/g" /pyqrack-jupyter/shor.py
-	ipython3 /pyqrack-jupyter/shor.py > factoring
-	cat factoring | grep 'Factors found'
-	echo 'Prime #1:   ' $prime 'Prime #2:   ' $prime2 'Multiplied: ' $fact
-	echo ' '
-    done
-done
+		prime=$(matho-primes -c 1 -u $i)
+		echo $(($prime + $prime)) > i2
+		i2=$(<i2)
+		prime2=$(matho-primes -c 1 -u $i2)
+		echo $(($prime * $prime2)) > fact
+		fact=$(<fact)
+		sed -i "s/toFactor=.*/toFactor=$fact/g" /pyqrack-jupyter/shor.py
+		ipython3 /pyqrack-jupyter/shor.py > factoring
+		cat factoring | grep 'Factors found'
+		echo 'Prime #1:   ' $prime 'Prime #2:   ' $prime2 'Multiplied: ' $fact
+		echo ' '
+	done
