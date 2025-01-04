a116427f961e840bf906b6bc31254d3599e4f5cd
Aryan Blaauw <ablaauw@gmx.net>
2025-01-04 13:08:46 +0000

    Create run-findafactor.sh

---
+++ runfiles/run-findafactor.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for i in {65535..4294967295..65535}; do
+  prime=$(matho-primes -c 1 -u $i)
+  i2=$((prime + prime))
+  prime2=$(matho-primes -c 1 -u $i2)
+  fact=$((prime * prime2))
+  python3 findafactor.py $fact
+done
