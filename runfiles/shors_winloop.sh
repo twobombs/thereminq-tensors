43b2a293d6a0b0ca6fd476be20f5cf9c4a1b4a32
Aryan Blaauw <ablaauw@gmx.net>
2025-01-04 14:46:44 +0000

    Update shors_winloop.sh

--- runfiles/shors_winloop.sh
+++ runfiles/shors_winloop.sh
@@ -6,8 +6,8 @@ for i in {65535..4294967295..255}
 		prime2=$(matho-primes -c 1 -u $i2)
 		echo $(($prime * $prime2)) > fact
 		fact=$(<fact)
-		sed -i "s/toFactor=.*/toFactor=$fact/g" /pyqrack-jupyter/shor.py
-		ipython3 /pyqrack-jupyter/shor.py > factoring
+		sed -i "s/toFactor=.*/toFactor=$fact/g" /notebook/qrack/pyqrack-jupyter/shor.py
+		ipython3 /notebook/qrack/pyqrack-jupyter/shor.py > factoring
 		cat factoring | grep 'Factors found'
 		echo 'Prime #1:   ' $prime 'Prime #2:   ' $prime2 'Multiplied: ' $fact
 		echo ' '
